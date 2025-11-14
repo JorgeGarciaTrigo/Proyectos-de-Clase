9ba3da722141a8b3714f4ed908ce85bf52512fe5
JorgeGarciaTrigo <jorgegartri@gmail.com>
2025-11-14 01:45:35 +0100

    push

--- Proyecto1_login/Procesar_Login1.php
+++ Proyecto1_login/Procesar_Login1.php
@@ -1,40 +1,41 @@
 <?php
+session_start();
 
-    echo "<link rel='stylesheet' href='ContenidoPL1.CSS'>";
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
 
-    echo "<body>";
+$fichero = file("Usuarios1.txt", FILE_IGNORE_NEW_LINES);
 
-    session_start();
-
-    $usuario = $_POST['usuario'];
-    $password = $_POST['password'];
-
-    $fichero = file("Usuarios1.txt", FILE_IGNORE_NEW_LINES);
-
-    $login_exitoso = false;
-    foreach ($fichero as $linea) {
-        list($user, $hash) = explode(":",$linea);
-        if ($user === $usuario && password_verify($password, $hash)) {
-            $login_exitoso = true;
-            $_SESSION['usuario'] = $usuario;
-            break;
-        }
+$login_exitoso = false;
+foreach ($fichero as $linea) {
+    list($user, $hash) = explode(":", $linea);
+    if ($user === $usuario && password_verify($password, $hash)) {
+        $login_exitoso = true;
+        $_SESSION['usuario'] = $usuario;
+        break;
     }
-    
-    if ($login_exitoso) {
-        header("Location:Bienvenida1.php");
-        exit;
-    } else {
+}
 
-        echo "<div class=Titulo1>";
-        echo "<h1>Usuario o contraseña incorrectos</h1>";
-        echo "</div>";
-
-        echo "<div class=Cuenta1>";
-        echo "<p><a href='Login1.php'>Volver a intentarlo</a></p>";
-        echo "</div>";
-    }
-
-    echo "</body>";
-
+if ($login_exitoso) {
+    header("Location: Bienvenida1.php");
+    exit;
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="ContenidoPL1.css">
+</head>
+<body>
+
+<div class="Titulo1">
+    <h1>Usuario o contraseña incorrectos</h1>
+</div>
+
+<div class="Cuenta1">
+    <p><a href="Login1.php">Volver a intentarlo</a></p>
+</div>
+
+</body>
+</html>
