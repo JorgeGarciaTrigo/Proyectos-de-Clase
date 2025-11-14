386129ac3c5c4631719f3efe2d4b23ed64ea9718
JorgeGarciaTrigo <jorgegartri@gmail.com>
2025-11-14 13:06:16 +0100

    push

--- Proyecto1_login/Procesar_Registro1.php
+++ Proyecto1_login/Procesar_Registro1.php
@@ -8,20 +8,16 @@ $password = $_POST['password'];
 
 $archivo = "Usuarios1.txt";
 
-// Si no existe, crearlo vacío
 if (!file_exists($archivo)) {
     file_put_contents($archivo, "");
 }
 
-// Leer archivo completo
 $lineas = file($archivo, FILE_IGNORE_NEW_LINES);
 
 $existe = false;
 
-// Comprobar si el usuario ya está registrado
 foreach ($lineas as $linea) {
 
-    // Evita líneas vacías o mal formadas
     if (trim($linea) === "") continue;
 
     list($user, $hash) = explode(":", $linea);
@@ -46,7 +42,6 @@ if ($existe) {
     exit;
 }
 
-// Registrar usuario nuevo
 $file = fopen($archivo, "a");
 fwrite($file, $usuario . ":" . password_hash($password, PASSWORD_DEFAULT) . "\n");
 fclose($file);
